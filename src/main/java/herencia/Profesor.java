ea45cb61f3cd2633222019a7fe0707e2e12391ca
Ivanok <ivan243117@gmail.com>
2025-11-25 17:48:32 -0500

    Agregar clases Estudiante y Profesor

---
+++ src/main/java/herencia/Profesor.java
@@ -0,0 +1,16 @@
+package herencia;
+
+public class Profesor extends Persona {
+    private String materia;
+
+    public Profesor(String nombre, int edad, String materia) {
+        super(nombre, edad);
+        this.materia = materia;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.println("Materia: " + materia);
+    }
+}
