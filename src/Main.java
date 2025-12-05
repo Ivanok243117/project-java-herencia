5fe10c6c31666d529850962fc6e26fb776e00db7
Ivanok <ivan243117@gmail.com>
2025-12-05 13:46:30 -0500

    feat: implementar Neumatico con verificarPresion

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,10 @@ public class Main {
         Transmision transmision = new Transmision("T001", "ZF", "Automática", 6);
         transmision.mostrarInformacion();
         transmision.cambiarMarcha(3);
+
+        System.out.println("\n==== NEUMÁTICO ====");
+        Neumatico neumatico = new Neumatico("N001", "Michelin", 17, 32.5);
+        neumatico.mostrarInformacion();
+        neumatico.verificarPresion();
     }
 }
