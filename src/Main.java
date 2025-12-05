f772103c5d4cc68a27063e14a8bd0362b11b73d5
Ivanok <ivan243117@gmail.com>
2025-12-05 13:23:18 -0500

    feat: creación del repositorio base para proyecto de herencia

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+//package com.ivanok.vehiculos;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Proyecto project-java-herencia: entorno listo.");
+    }
+}
